cda9780e0944c8913843aabd2da2ac05ae3b1d9c
Leila Rosa <leila_rosa3@outlook.com>
2022-03-31 10:37:27 -0300

    login completo, dashboard em andamento

---
+++ cms/dashboard.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <main>
+        <div class="container-banner">
+            <div class="container-titulos">
+                <h1>CMS - Projeto Games</h1>
+                <h2>Gerenciamento do conteúdo do site</h2>
+            </div>
+            <div class="container-img-logo">
+                <img src="" alt="">
+            </div>
+        </div>
+        <div class="container-banner-opcoes">
+            <div class="container-sessoes">
+                <div class="container-opcoes">
+                    <div class="container-icon">
+                        <img src="" alt="">
+                    </div>
+                    <p>Adm. de Produtos</p>
+                </div>
+                <div class="container-opcoes">
+                    <div class="container-icon">
+                        <img src="" alt="">
+                    </div>
+                    <p>Adm. de Categorias</p>
+                </div>
+                <div class="container-opcoes">
+                    <div class="container-icon">
+                        <img src="" alt="">
+                    </div>
+                    <p>Contatos</p>
+                </div>
+                <div class="container-opcoes">
+                    <div class="container-icon">
+                        <img src="" alt="">
+                    </div>
+                    <p>Usuários</p>
+                </div>
+            </div>
+            <div class="container-infos">
+                <p>Bem-vindo {nome}</p>
+                <div class="container-opcoes">
+                    <div class="container-icon">
+                        <img src="" alt="">
+                    </div>
+                    <p>Logout</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-sessao">
+            <h3>Titulo da sessão</h1>
+        </div>
+    </main>
+    <footer>
+        <span>Desenvolvido por Leila Rosa. Versão 1.0</span>
+    </footer>
+</body>
+</html>
